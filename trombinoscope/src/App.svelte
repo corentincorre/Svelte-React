d38e43686cd1faf4f9b31430114c9273a723276f
Corentin Corre <corentin.corre@hotmail.fr>
2023-02-17 14:09:52 +0100

    test

--- trombinoscope/src/App.svelte
+++ trombinoscope/src/App.svelte
@@ -1,48 +1,165 @@
 <script>
-    import Slider from "./lib/Slider.svelte";
-    import Book from "./lib/Book.svelte";
-    import Movie from "./lib/Movie.svelte";
-    import Song from "./lib/Song.svelte";
-    const items = [
+    import Trombi from "./lib/Trombi.svelte";
+    let recherche = "";
+    let age = 0;
+    let alphabetical = false;
+    let alive = false;
+    const persons = [
         {
-            name: "Harry Potter",
-            director: "Alfonso Cuaron",
-            type: "movie",
+            name: "Romain",
+            surname: "l'Ourson",
+            born: "1985-12-31",
+            description: "Someone who loves Svelte !",
         },
         {
-            name: "Harry Potter",
-            author: "JK Rowling",
-            type: "book",
+            name: "Harry",
+            surname: "Potter",
+            born: "1980-07-31",
+            description: "A wizard who defeated He-who-must-not-be-named",
         },
         {
-            name: "Star Wars",
-            director: "Georges Lucas",
-            type: "movie",
+            name: "Steve",
+            surname: "Jobs",
+            born: "1955-02-24",
+            dead: "2011-10-05",
+            description: "Someone who loved apples",
         },
         {
-            name: "Hey Jude",
-            band: "The Beatles",
-            type: "song",
+            name: "Diego",
+            surname: "Maradona",
+            born: "1960-10-30",
+            dead: "2020-11-25",
+            description: "The hand of God",
         },
         {
-            name: "La bamba",
-            band: "Les Gipsy Kings",
-            type: "song",
+            name: "Osamu",
+            surname: "Tezuka",
+            born: "1928-11-03",
+            dead: "1989-02-09",
+            description: "Astro boy's dad",
         },
         {
-            name: "Les fleurs du mal",
-            author: "Charles Baudelaire",
-            type: "book",
+            name: "Billie",
+            surname: "Eilish",
+            born: "2001-12-18",
+            description: "Happier then ever",
+        },
+        {
+            name: "Bernard",
+            surname: "Minet",
+            born: "1953-12-28",
+            description: "Un mec musclé",
         },
     ];
+    let personsWithAge = persons;
+    personsWithAge.forEach((element) => {
+        element.age = calculAge(element);
+    });
+    let personsToDisplay = personsWithAge;
+
+    function calculAge(person) {
+        const born = new Date(person.born);
+        const end = person.dead ? new Date(person.dead) : new Date();
+        let calc = end.getTime() - born.getTime();
+        let age = new Date(calc);
+        return age.getFullYear() - 1970;
+    }
+    $: personsToDisplayFiltered = applyFilter(
+        personsToDisplay,
+        recherche,
+        age,
+        alphabetical,
+        alive
+    );
+    function applyFilter(array, recherche, age, alphabetical, alive) {
+        let res = [];
+        array.forEach((element) => {
+            if (element.age > age) {
+                if (
+                    element.name
+                        .toLowerCase()
+                        .includes(recherche.toLowerCase()) ||
+                    element.surname
+                        .toLowerCase()
+                        .includes(recherche.toLowerCase())
+                ) {
+                    res.push(element);
+                }
+            }
+        });
+        return res;
+    }
+
+    function filterByName(array, filter) {
+        personsToDisplay = [];
+        array.forEach((element) => {
+            if (
+                element.name.toLowerCase().includes(filter.toLowerCase()) ||
+                element.surname.toLowerCase().includes(filter.toLowerCase())
+            ) {
+                personsToDisplay.push(element);
+            }
+        });
+    }
+    function reset() {
+        personsToDisplay = persons;
+        recherche = "";
+        age = 0;
+        alphabetical = false;
+        alive = false;
+    }
 </script>
 
-<Slider {items} let:item>
-    {#if item.type === "book"}
-        <Book name={item.name} author={item.author} />
-    {:else if item.type === "movie"}
-        <Movie name={item.name} director={item.director} />
-    {:else if item.type === "song"}
-        <Song name={item.name} band={item.band} />
-    {/if}
-</Slider>
+<nav class="filters">
+    <button
+        class:active={alphabetical}
+        on:click={() => (alphabetical = !alphabetical)}
+    >
+        Par ordre alphabétique
+    </button>
+    <input
+        type="text"
+        bind:value={recherche}
+        on:input={(e) => filterByName(persons, recherche)}
+    />
+    <input
+        type="range"
+        name="age"
+        bind:value={age}
+        min="0"
+        max="100"
+        step="1"
+    />
+    <label for="age">Age minimum : {age}</label>
+
+    <button class:active={alive} on:click={() => (alive = !alive)}>
+        Seulement les vivants
+    </button>
+
+    <button on:click={reset}>Réinitialiser les filtres</button>
+</nav>
+
+<Trombi items={personsToDisplayFiltered} />
+
+<style>
+    .filters {
+        display: flex;
+        justify-content: center;
+        gap: 10px;
+        align-items: center;
+        height: 80px;
+        background-color: white;
+        box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.25);
+    }
+    button {
+        border: none;
+        background-color: aliceblue;
+        border-radius: 5px;
+        padding: 5px 10px;
+        box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.25);
+    }
+    .active {
+        background-color: #5555ff;
+        color: white;
+    }
+</style>
